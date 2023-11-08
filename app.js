43bf45a783548f63e407334817d12b061cb7e67e
Mentabomber <mentae_mentology@libero.it>
2023-11-08 15:29:55 +0100

    initialization and tasks until n.4

---
+++ app.js
@@ -0,0 +1,119 @@
+const moviesAndShows = [
+{
+    title: "Il Signore degli Anelli: La Compagnia dell'Anello",
+    year: 2001,
+    genre: "Fantasy",
+    rating: 9.2,
+    type: "movie"
+
+},
+{
+    title: "Breaking Bad",
+    year: 2008,
+    genre: "Drama",
+    rating: 9.5,
+    type: "tv",
+    seasons: 5
+
+},
+{
+    title: "La forma dell'acqua",
+    year: 2017,
+    genre: "Fantasy",
+    rating: 7.3,
+    type: "movie"
+},
+{
+    title: "Game of Thrones",
+    year: 2011,
+    genre: "Fantasy",
+    rating: 9.3,
+    type: "tv",
+    seasons: 8
+},
+{
+    title: "Titanic",
+    year: 1997,
+    genre: "Drama",
+    rating: 7.8,
+    type: "movie"
+},
+{
+    title: "Stranger Things",
+    year: 2016,
+    genre: "Science Fiction",
+    rating: 8.7,
+    type: "tv",
+    seasons: 4
+},
+{
+    title: "Inception",
+    year: 2010,
+    genre: "Science Fiction",
+    rating: 8.8,
+    type: "movie"
+},
+{
+    title: "Friends",
+    year: 1994,
+    genre: "Comedy",
+    rating: 8.5,
+    type: "tv",
+    seasons: 10
+},
+{
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genre: "Drama",
+    rating: 9.3,
+    type: "movie"
+},
+{
+    title: "The Crown",
+    year: 2016,
+    genre: "Drama",
+    rating: 8.6,
+    type: "tv",
+    seasons: 5
+}
+]
+
+class Movie {
+    title;
+    year;
+    genre;
+    rating;
+    type;
+    constructor (title, year, genre, rating, type) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rating;
+        this.type = type;
+
+    }
+    toString(){
+        return `${this.title} è un film di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}`;
+    }
+}
+
+class TvSeries extends Movie {
+    seasons;
+
+    constructor (title, year, genre, rating, type, seasons) {
+        super(title, year, genre, rating, type);
+        this.seasons = seasons;
+
+    }
+    toString(){
+        return `${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}`;
+    }
+}
+
+
+const show1 = new Movie(moviesAndShows[0].title,moviesAndShows[0].year,moviesAndShows[0].genre,moviesAndShows[0].rating,moviesAndShows[0].type);
+let show2 = new Movie("titanic","1999","drama","8","movie")
+let show3 = new TvSeries("Better Call Saul", 2015, "drama", 9, "tv", 6)
+console.log(show1.toString());
+console.log(show2.toString());
+console.log(show3.toString());
